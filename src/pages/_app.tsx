95f4bbb64874c10b971b0502f3ad81b73e15591d
Hugo Leonardo <hugo.leonardo@arizona.global>
2021-07-22 09:12:36 -0300

    feat: create the Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import '../styles/global.scss';
+import { Header } from '../components/Header';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <Component {...pageProps} />
-  </>
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
   )
 }
 
